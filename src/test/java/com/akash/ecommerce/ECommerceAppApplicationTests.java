c6482f3aaa63777171fbc149abc79a382317cbb0
Akash Sahu <akkusahu559@gmail.com>
2021-06-26 08:56:08 +0530

    App file added

---
+++ src/test/java/com/akash/ecommerce/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
