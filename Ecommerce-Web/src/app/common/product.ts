d3824fc648c33f938bc9b3e56d3afbc707509d38
Akash Sahu <akkusahu559@gmail.com>
2021-06-26 09:50:06 +0530

    Frontend folder renamed

---
+++ Ecommerce-Web/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: String;
+    name: String;
+    description: String;
+    unitPrice: number;
+    imageUrl: String;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+  }
