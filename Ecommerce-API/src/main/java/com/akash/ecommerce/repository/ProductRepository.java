8af9d988414ab2b5563841a0c7d44f0649147e12
Akash Sahu <akkusahu559@gmail.com>
2021-06-28 07:25:07 +0530

    API Added for getting the result by Category

--- Ecommerce-API/src/main/java/com/akash/ecommerce/repository/ProductRepository.java
+++ Ecommerce-API/src/main/java/com/akash/ecommerce/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.akash.ecommerce.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.akash.ecommerce.entity.Product;
 
@@ -10,4 +13,5 @@ import com.akash.ecommerce.entity.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
