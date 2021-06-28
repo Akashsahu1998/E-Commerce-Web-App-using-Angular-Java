8eba173b382f6d9fd549dcd8dfaf73b1b894b1a0
Akash Sahu <akkusahu559@gmail.com>
2021-06-28 07:23:42 +0530

    Routing added for the URL's

--- Ecommerce-Web/src/app/app-routing.module.ts
+++ Ecommerce-Web/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
