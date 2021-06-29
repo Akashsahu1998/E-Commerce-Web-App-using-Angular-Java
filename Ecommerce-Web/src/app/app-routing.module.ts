b71ccbba9dcba64918b6285271aa1c8b80a12d3b
Akash Sahu <akkusahu559@gmail.com>
2021-06-29 06:37:36 +0530

    Product category name display updated

--- Ecommerce-Web/src/app/app-routing.module.ts
+++ Ecommerce-Web/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
-  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category/:id/:name', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
--- Ecommerce-Web/src/app/components/product-list/product-list.component.ts
+++ Ecommerce-Web/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
@@ -30,15 +31,19 @@ export class ProductListComponent implements OnInit {
     if(hasCategoryId){
       // get the "id" param string and convert to a number using the '+' symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+      
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     }
     else{
       // not category id available, default to category id is 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = "Books";
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(data => {
       this.products = data;
-    })    
+    })
   }
 }
 
