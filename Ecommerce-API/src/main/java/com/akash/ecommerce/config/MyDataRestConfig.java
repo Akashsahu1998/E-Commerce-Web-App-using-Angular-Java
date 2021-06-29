0df19b07ab061ec6bb92e09c00452a0f06588df7
Akash Sahu <akkusahu559@gmail.com>
2021-06-29 06:10:50 +0530

    MyDataRestConfig updated for exposing the entity Id's

--- Ecommerce-API/src/main/java/com/akash/ecommerce/config/MyDataRestConfig.java
+++ Ecommerce-API/src/main/java/com/akash/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,13 @@
 package com.akash.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,6 +19,13 @@ import com.akash.ecommerce.entity.ProductCategory;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -27,8 +42,29 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		config.getExposureConfiguration().forDomainType(ProductCategory.class)
 				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions))
 				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions));
+
+		// call an internal helper method to expose the ID's
+		exposeIds(config);
 	}
 
+	// expose entity ID's
+	private void exposeIds(RepositoryRestConfiguration config) {
+
+		// get a list of all entity class from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+		// create an array of the entity types
+		List<Class> entityClasses = new ArrayList<>();
+
+		// get the entity types for the entities
+		for (EntityType<?> entityType : entities) {
+			entityClasses.add(entityType.getJavaType());
+		}
+
+		// expose the entity ID's for the array of entity/domain types
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
+	}
 }
 
 // With the help of this class we are making the REST API as read only 
