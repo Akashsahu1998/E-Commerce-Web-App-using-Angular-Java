2696e9d44056cef5454a38bebd73c73a89d5560e
Akash Sahu <akkusahu559@gmail.com>
2021-06-30 04:50:06 +0530

    Search method updated for searching the product by category

--- Ecommerce-API/src/main/java/com/akash/ecommerce/repository/ProductRepository.java
+++ Ecommerce-API/src/main/java/com/akash/ecommerce/repository/ProductRepository.java
@@ -14,4 +14,6 @@ import com.akash.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
