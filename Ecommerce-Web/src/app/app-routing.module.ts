62cfec6c4e0549c9f206c5c368a49586b5d778d9
Akash Sahu <akkusahu559@gmail.com>
2021-07-01 05:41:51 +0530

    Updated the routing for the product details and also added the id as a parameter into the class

--- Ecommerce-Web/src/app/app-routing.module.ts
+++ Ecommerce-Web/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
-  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},  
   {path: 'category/:id', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
--- Ecommerce-Web/src/app/common/product.ts
+++ Ecommerce-Web/src/app/common/product.ts
@@ -8,4 +8,5 @@ export class Product {
     unitsInStock: number;
     dateCreated: Date;
     lastUpdated: Date;
+    id: number;
   }
