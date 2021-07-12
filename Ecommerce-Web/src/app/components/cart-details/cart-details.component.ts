db734a4d2a38377fa85bf21dc568d7b4d4229875
Akash Sahu <akkusahu559@gmail.com>
2021-07-12 07:16:19 +0530

    Cart-Details component functionality added

--- Ecommerce-Web/src/app/components/cart-details/cart-details.component.ts
+++ Ecommerce-Web/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails(){
+
+    // get a handle to the cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // subscribe to the cart Total Price
+    this.cartService.totalPrice.subscribe(data => this.totalPrice = data)
+        
+    // subscribe to the cart Total Quantity
+    this.cartService.totalPrice.subscribe(data => this.totalQuantity = data)
+        
+    // subscribe to the cart Total Price abd Quantity
+    this.cartService.computeCartTotals();
   }
 
 }
