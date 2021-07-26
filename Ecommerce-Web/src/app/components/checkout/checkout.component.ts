9dbfb62eb5cb525699d91513343c7429a5a1cc63
Akash <akkusahu559@gmail.com>
2021-07-26 09:49:03 +0530

    Populated the month and year in credit card expiration

--- Ecommerce-Web/src/app/components/checkout/checkout.component.ts
+++ Ecommerce-Web/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ShopFormService } from 'src/app/services/shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,7 +14,10 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder, private shopFormService: ShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -45,19 +49,37 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+
+    // populate credit card months
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startmonth : " + startMonth);
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(data => {
+      console.log("credit month : " + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+
+    // populate credit card years
+
+    this.shopFormService.getCreditCardYears().subscribe(data => {
+      console.log("credit year : " + JSON.stringify(data));
+      this.creditCardYears = data;
+    })
+
   }
 
-  copyShippingAddressToBillingAddress(event: any){
-    if(event.target.checked){
+  copyShippingAddressToBillingAddress(event: any) {
+    if (event.target.checked) {
       this.checkoutFormGroup.controls.billingAddress
-          .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+        .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
     }
-    else{
+    else {
       this.checkoutFormGroup.controls.billingAddress.reset();
     }
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log("Handling");
     console.log(this.checkoutFormGroup.get('customer')!.value);
     console.log(this.checkoutFormGroup.get('customer')!.value.email);
