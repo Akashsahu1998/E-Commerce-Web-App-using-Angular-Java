21ec7a115e84a8956251a48fb828752c9431d29e
Akash <akkusahu559@gmail.com>
2021-07-28 08:08:05 +0530

    state class is created

---
+++ Ecommerce-API/src/main/java/com/akash/ecommerce/entity/State.java
@@ -0,0 +1,32 @@
+package com.akash.ecommerce.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "state")
+@Getter
+@Setter
+public class State {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+}
