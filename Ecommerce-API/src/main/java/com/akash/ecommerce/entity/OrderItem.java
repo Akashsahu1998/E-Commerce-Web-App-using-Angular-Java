ce318cead35ef96f2a0e08bdc08cca1acb3d0c0d
Akash <akkusahu559@gmail.com>
2021-08-10 06:19:59 +0530

    OrderItem table created

---
+++ Ecommerce-API/src/main/java/com/akash/ecommerce/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package com.akash.ecommerce.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	@Column(name = "unit_price")
+	private BigDecimal unitPrice;
+
+	@Column(name = "quantity")
+	private int quantity;
+
+	@Column(name = "product_id")
+	private long productId;
+}
