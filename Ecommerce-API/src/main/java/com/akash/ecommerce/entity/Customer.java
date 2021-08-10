53a824f42635542758066903c5e92d164faba9de
Akash <akkusahu559@gmail.com>
2021-08-10 06:09:39 +0530

    Customer table created

---
+++ Ecommerce-API/src/main/java/com/akash/ecommerce/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.akash.ecommerce.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+}
