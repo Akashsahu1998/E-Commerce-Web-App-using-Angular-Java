0d1f9243b343a5ec33dd264bf83e2fcc9cc15eea
Akash <akkusahu559@gmail.com>
2021-09-27 18:57:50 +0530

    app config file added for octa setup

---
+++ Ecommerce-Web/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+    oidc: {
+        clientId: '0oa20s2wadhsX0Eqn5d7',   // public identifier of the client app
+        issuer: 'http://dev-91861588.okta.com/oauth2/default',  // issuer of tokens, URL when authorizing with Okta Authorization Server
+        redirectUri: 'http://localhost:4200/login/callback',    // Once user logs in send them here
+        scope: ['openid', 'profile', 'email']   // Scopes provides access to information about a user
+    }                                               // openid : required for authentication requests 
+}                                               // profile : user's first name, last name, phone etc
+                                                // email : user's email address
+
